032be37865c582fde28faafd340cf1c42c22636c
Manos Fountoulakis <fountoulakismanos@gmail.com>
2019-01-04 01:07:10 -0500

    show team triCode

--- components/ActiveScore/index.js
+++ components/ActiveScore/index.js
@@ -7,19 +7,20 @@ import Text from '../Text';
 class ActiveScore extends React.Component {
   render() {
     const { activeGame = {} } = this.props;
-    console.log(activeGame);
 
     return (
       <Flex justifyContent="space-between">
         <Flex flexDirection={'column'} alignItems={'center'}>
           <Text fontSize={4}>{activeGame.hTeam.score}</Text>
           <Text color={'gray'} fontSize={0}>
+            {activeGame.hTeam.triCode}
             {activeGame.hTeam.record}
           </Text>
         </Flex>
         <Flex flexDirection={'column'} alignItems={'center'}>
           <Text fontSize={4}>{activeGame.vTeam.score}</Text>
           <Text color={'gray'} fontSize={0}>
+            {activeGame.vTeam.triCode}
             {activeGame.vTeam.record}
           </Text>
         </Flex>
