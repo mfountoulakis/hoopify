7f0d139ab6aedea38cd3c412371f5e53bd32d8a8
Jesse Hoyos <jesse@mojotech.com>
2019-01-08 21:26:35 -0500

    [refactor] use new components for teams page

--- pages/teams.js
+++ pages/teams.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-// import Button from '../components/Button';
 import fetch from 'isomorphic-unfetch';
 import PropTypes from 'prop-types';
 import getConfig from 'next/config';
-import TeamPicker from '../components/TeamPicker';
 import { withRouter } from 'next/router';
+
+import { ViewLayout } from '../components/Layout';
+import ActionButton from '../components/ActionButton';
+import Text from '../components/Text';
+import Select from '../components/Select';
 
 class Teams extends Component {
   static async getInitialProps() {
@@ -21,6 +24,8 @@ class Teams extends Component {
     localStorage.setItem('favTeam', team);
   };
 
+  handleChange = event => this.setFavoriteTeam(event.target.value);
+
   render() {
     const {
       router,
@@ -32,12 +37,29 @@ class Teams extends Component {
     const Nba = standardTeams =>
       standardTeams.filter(team => team.isNBAFranchise === true);
 
+    const standardTeams = Nba(standard);
+
     return (
-      <TeamPicker
-        handleClick={() => router.push('/')}
-        handleChange={e => this.setFavoriteTeam(e.target.value)}
-        teams={Nba(standard)}
-      />
+      <ViewLayout
+        bottom={
+          <ActionButton onClick={() => router.push('/')}>GO</ActionButton>
+        }
+      >
+        <Text fontSize={3} as={'label'} htmlFor={'team-picker'}>
+          Choose your favorite NBA team to start:
+        </Text>
+        <Select htmlName={'team-picker'} onChange={e => this.handleChange(e)}>
+          {standardTeams.map(team => (
+            <option value={team.tricode} key={team.teamId}>
+              {team.fullName}
+            </option>
+          ))}
+        </Select>
+        <Text color={'gray'}>
+          Free agent fan? Don’t stress it, you can choose a different team later
+          in the settings.
+        </Text>
+      </ViewLayout>
     );
   }
 }
