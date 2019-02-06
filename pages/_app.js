17e6ee49bbd7e1e711b4daf844e59919e407cbca
Manos Fountoulakis <fountoulakismanos@gmail.com>
2019-02-05 22:57:54 -0500

    adds FavContext Provider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import FavContext from '../context/FavContext';
 import theme from '../lib/theme';
 
 export default class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favTeam: '',
+      loading: true,
+    };
+  }
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
@@ -14,12 +22,30 @@ export default class MyApp extends App {
     return { pageProps };
   }
 
+  componentDidMount() {
+    if (window.localStorage) {
+      const favTeam = localStorage.getItem('favTeam');
+      this.setState({ favTeam, loading: false });
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
+
     return (
       <Container>
         <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
+          <FavContext.Provider value={this.state.favTeam}>
+            <Component
+              {...pageProps}
+              {...this.state}
+              setFavTeam={v =>
+                this.setState({ favTeam: v }, () => {
+                  localStorage.setItem('favTeam', this.state.favTeam);
+                })
+              }
+            />
+          </FavContext.Provider>
         </ThemeProvider>
       </Container>
     );
