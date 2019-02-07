16118d27ed910e1bbe6e24b1bb3e4ad86bb341ea
Jesse Hoyos <jesse@mojotech.com>
2019-02-07 00:49:35 -0500

    [refactor] Views render Navigation component by default

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Flex } from '@rebass/grid';
 
+import Navigation from '../Navigation';
+
 export const MaxBound = props => <Box px={2} mx={'auto'} {...props} />;
 
 const bottomCss = {
@@ -13,18 +15,22 @@ const bottomCss = {
   width: '100%',
 };
 
-export const ViewLayout = props => {
+export const View = props => {
   return (
     <Box>
       <Flex pt={6}>
         <MaxBound>{props.children}</MaxBound>
       </Flex>
-      {props.bottom && <Flex css={bottomCss}>{props.bottom}</Flex>}
+      {props.bottom ? (
+        <Flex css={bottomCss}>{props.bottom}</Flex>
+      ) : (
+        <Navigation />
+      )}
     </Box>
   );
 };
 
-ViewLayout.propTypes = {
+View.propTypes = {
   children: PropTypes.node.isRequired,
   bottom: PropTypes.node,
 };
