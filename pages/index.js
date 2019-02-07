c61be44fcdb937459512f5f1709747e5d5f3c249
Manos Fountoulakis <fountoulakismanos@gmail.com>
2019-02-06 20:05:35 -0500

    move fetching out of index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import fetch from 'isomorphic-unfetch';
 import PropTypes from 'prop-types';
-import getConfig from 'next/config';
 import { ViewLayout } from '../components/Layout';
 import Link from 'next/link';
-import { compose, filter, map, prop } from 'lodash/fp';
 import teamNames from '../lib/teamNames';
 import { withRouter } from 'next/router';
 import Text from '../components/Text';
@@ -15,58 +12,6 @@ class Index extends Component {
     this.state = {
       promptEvent: null,
     };
-  }
-
-  static async getInitialProps() {
-    const { publicRuntimeConfig } = getConfig();
-    const url = `${publicRuntimeConfig.BASEURL}`;
-    const result = await fetch(`${url}/api/today`);
-    const json = await result.json();
-
-    return {
-      games: json.games,
-    };
-  }
-
-  filterFavorite = () => {
-    const { games, router, favTeam } = this.props;
-
-    const mapGame = xs =>
-      xs.map(xs => {
-        return {
-          id: xs.gameId,
-          matchup: [xs.hTeam.triCode, xs.vTeam.triCode],
-        };
-      });
-
-    const teamPlayingToday = compose(
-      map(prop('id')),
-      filter({ matchup: [favTeam] }),
-      mapGame,
-    );
-    teamPlayingToday(games).length
-      ? router.push(`/game/${teamPlayingToday(games)}`)
-      : null;
-  };
-
-  componentDidMount() {
-    const { favTeam, router } = this.props;
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('beforeinstallprompt', e => {
-        e.preventDefault(); // Prevents prompt display initially
-        this.setState({ promptEvent: e });
-      });
-    }
-    if (!favTeam) {
-      router.push('/teams');
-    }
-    this.filterFavorite();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.favTeam !== prevProps.favTeam) {
-      this.filterFavorite();
-    }
   }
 
   render() {
