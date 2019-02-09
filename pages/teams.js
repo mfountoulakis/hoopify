4ae05ec325e3b28d8a80d2fa28d90720f4ae8b05
Manos Fountoulakis <fountoulakismanos@gmail.com>
2019-02-09 18:44:16 -0500

    removes unused imports from teams

--- pages/teams.js
+++ pages/teams.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
 import PropTypes from 'prop-types';
 import getConfig from 'next/config';
-import { ViewLayout } from '../components/Layout';
-import ActionButton from '../components/ActionButton';
 import Text from '../components/Text';
 import Select from '../components/Select';
 
@@ -31,27 +29,23 @@ class Teams extends Component {
       standardTeams.filter(team => team.isNBAFranchise === true);
 
     const standardTeams = Nba(standard);
-
     return (
-      <ViewLayout
-        bottom={
-          <ActionButton onClick={() => this.props.filterFavorite()}>GO</ActionButton>
-        }>
-        <Text fontSize={3} mb={4} as={'label'} htmlFor={'team-picker'}>
-          Choose your favorite NBA team to start:
-        </Text>
-        <Select htmlName={'team-picker'} onChange={e => this.handleChange(e)}>
-          {standardTeams.map(team => (
-            <option value={team.tricode} key={team.teamId}>
-              {team.fullName}
-            </option>
-          ))}
-        </Select>
-        <Text color={'gray'}>
-          Free agent fan? Don’t stress it, you can choose a different team later
-          in the settings.
-        </Text>
-      </ViewLayout>
+      <div>
+          <Text fontSize={3} mb={4} as={'label'} htmlFor={'team-picker'}>
+            Choose your favorite NBA team to start:
+          </Text>
+          <Select htmlName={'team-picker'} onChange={e => this.handleChange(e)}>
+            {standardTeams.map(team => (
+              <option value={team.tricode} key={team.teamId}>
+                {team.fullName}
+              </option>
+            ))}
+          </Select>
+          <Text color={'gray'}>
+            Free agent fan? Don’t stress it, you can choose a different team later
+            in the settings.
+          </Text>
+      </div>
     );
   }
 }
