f4177e519626da64f543a602a57702c4130e21cf
Manos Fountoulakis <fountoulakismanos@gmail.com>
2019-02-09 18:48:26 -0500

    adds required function

--- pages/teams.js
+++ pages/teams.js
@@ -31,29 +31,29 @@ class Teams extends Component {
     const standardTeams = Nba(standard);
     return (
       <div>
-          <Text fontSize={3} mb={4} as={'label'} htmlFor={'team-picker'}>
-            Choose your favorite NBA team to start:
-          </Text>
-          <Select htmlName={'team-picker'} onChange={e => this.handleChange(e)}>
-            {standardTeams.map(team => (
-              <option value={team.tricode} key={team.teamId}>
-                {team.fullName}
-              </option>
-            ))}
-          </Select>
-          <Text color={'gray'}>
-            Free agent fan? Don’t stress it, you can choose a different team later
-            in the settings.
-          </Text>
+        <Text fontSize={3} mb={4} as={'label'} htmlFor={'team-picker'}>
+          Choose your favorite NBA team to start:
+        </Text>
+        <Select htmlName={'team-picker'} onChange={e => this.handleChange(e)}>
+          {standardTeams.map(team => (
+            <option value={team.tricode} key={team.teamId}>
+              {team.fullName}
+            </option>
+          ))}
+        </Select>
+        <Text color={'gray'}>
+          Free agent fan? Don’t stress it, you can choose a different team later
+          in the settings.
+        </Text>
       </div>
     );
   }
 }
 
 Teams.propTypes = {
-  filterFavorite:PropTypes.func.isRequired,
+  filterFavorite: PropTypes.func.isRequired,
   teams: PropTypes.object.isRequired,
   setFavTeam: PropTypes.func.isRequired,
 };
 
-export default Teams
+export default Teams;
